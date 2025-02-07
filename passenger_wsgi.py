25c39a23378934f4ebf7f516d01d8ec4b1754548
Timi-G <56408619+Timi-G@users.noreply.github.com>
2025-02-07 21:48:17 +0100

    Delete passenger_wsgi.py

--- passenger_wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0,"")
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edstack.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
